419152ea2ccaef12cc8aad81e5c817d9fce8b865
kaulhj <lhj69382@naver.com>
2022-01-15 21:25:07 +0900

    first commit

---
+++ src/main/java/com/mumulcom/mumulcom/MumulcomApplication.java
@@ -0,0 +1,13 @@
+package com.mumulcom.mumulcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumulcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumulcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mumulcom/mumulcom/MumulcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumulcom.mumulcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MumulcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
