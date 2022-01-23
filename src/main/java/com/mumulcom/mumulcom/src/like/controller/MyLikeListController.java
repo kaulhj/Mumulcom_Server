a094d39a06d4388edc75e2cd5ad98dab3bb9b455
hwihwi99 <chamgrace@naver.com>
2022-01-23 20:00:39 +0900

    feat : 좋아요 목록 보여주기 기능 삭제

--- src/main/java/com/mumulcom/mumulcom/src/like/controller/MyLikeListController.java
+++
@@ -1,4 +0,0 @@
-package com.mumulcom.mumulcom.src.like.controller;
-
-public class MyLikeListController {
-}
--- src/main/java/com/mumulcom/mumulcom/src/like/provider/MyLikeListProvider.java
+++
@@ -1,4 +0,0 @@
-package com.mumulcom.mumulcom.src.like.provider;
-
-public class MyLikeListProvider {
-}
