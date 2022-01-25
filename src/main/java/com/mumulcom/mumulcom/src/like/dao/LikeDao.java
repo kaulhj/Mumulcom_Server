4dbe573a2714a7c77530787efadb2e7ab8daa947
kaulhj <lhj69382@naver.com>
2022-01-25 17:37:31 +0900

    refactor 좋아요api호출시알림테이블insert

--- src/main/java/com/mumulcom/mumulcom/src/like/dao/LikeDao.java
+++ src/main/java/com/mumulcom/mumulcom/src/like/dao/LikeDao.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 
@@ -21,12 +23,52 @@ public class LikeDao {
 
     public String createLike(PostLikeReq postLikeReq){
 
-            String createLikeQuery = "INSERT INTO `Like`(QUESTIONIDX, USERIDX) VALUES (?, ?)";
+            String createLikeQuery = "INSERT INTO `Like`(QUESTIONIDX, USERIDX,REPLYIDX) VALUES (?, ?,?)";
             Object[] createLikeParams = new Object[]{postLikeReq.getQuestionIdx(),
-            postLikeReq.getUserIdx()};
+            postLikeReq.getUserIdx(),postLikeReq.getReplyIdx()};
             this.jdbcTemplate.update(createLikeQuery,createLikeParams);
 
-            return new String("해당글을 좋아요 하였습니다");
+            String creLikNotQuery = "INSERT INTO Notice (noticeCategoryIdx,questionIdx,userIdx,noticeContent)\n" +
+                    "VALUES (?,?,?,?)";
+
+            int noticeCategory; //알림 카테고리 구하기
+             String lastContent = null;
+        if(postLikeReq.getReplyIdx() == 0) {
+                noticeCategory = 2;
+                lastContent = new String(" 질문에 좋아요를 눌렀습니다.");
+        }else {
+            noticeCategory = 5;
+            lastContent = new String(" 답변에 좋아요를 눌렀습니다.");
+        }
+
+
+        //
+            String nContentQuery = "SELECT U.name,Q.title\n" +
+                    "FROM `Like` L\n" +
+                    "INNER JOIN Question Q on L.questionIdx = Q.questionIdx\n" +
+                    "INNER JOIN User U on L.userIdx = U.userIdx" +
+                    " where L.questionIdx = ? AND L.userIdx = ?\n" +
+                    " AND L.replyIdx = ?";
+            Object[] nContentParams = new Object[]{postLikeReq.getQuestionIdx(),postLikeReq.getUserIdx(),
+            postLikeReq.getReplyIdx()};
+        //유저이름, 타이틀 저장
+        List<Map<String,Object>> nContentParam2 = this.jdbcTemplate.queryForList(nContentQuery,
+                nContentParams);
+
+
+        String nContent = new String(nContentParam2.get(0).get("name")+" 님이 회원님의 "+
+                nContentParam2.get(0).get("title")+lastContent);
+
+
+
+        Object[] creLikeNotParams = new Object[]{noticeCategory,postLikeReq.getQuestionIdx(),postLikeReq.getUserIdx(),
+            nContent};
+
+        this.jdbcTemplate.update(creLikNotQuery,creLikeNotParams);
+            return nContent;
 
     }
+
+
+
 }
