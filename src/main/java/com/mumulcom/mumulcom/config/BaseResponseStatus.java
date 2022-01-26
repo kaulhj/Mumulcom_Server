c07a55f58a4d0b39cfddecf7f9a9b57bdf133b8b
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-01-26 14:57:36 +0900

    refactor: POST_USERS_EXISTS_EMAIL, FAILED_TO_LOGIN 메시지 수정

--- src/main/java/com/mumulcom/mumulcom/config/BaseResponseStatus.java
+++ src/main/java/com/mumulcom/mumulcom/config/BaseResponseStatus.java
@@ -28,7 +28,7 @@ public enum BaseResponseStatus {
     // [POST] /users
     POST_USERS_EMPTY_EMAIL(false, 2015, "이메일을 입력해주세요."),
     POST_USERS_INVALID_EMAIL(false, 2016, "이메일 형식을 확인해주세요."),
-    POST_USERS_EXISTS_EMAIL(false,2017,"중복된 이메일입니다."),
+    POST_USERS_EXISTS_EMAIL(false,2017,"이미 가입된 이메일입니다."),
     POST_USERS_EXISTS_NICKNAME(false, 2018, "중복된 닉네임입니다."),
 
 
@@ -41,7 +41,7 @@ public enum BaseResponseStatus {
 
     // [POST] /users
     DUPLICATED_EMAIL(false, 3013, "중복된 이메일입니다."),
-    FAILED_TO_LOGIN(false,3014,"없는 아이디거나 비밀번호가 틀렸습니다."),
+    FAILED_TO_LOGIN(false,3014,"가입된 이메일이 아닙니다."),
 
 
 
