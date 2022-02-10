9802e4244de0e24fb494d0750f33700920538cbd
hwihwi99 <chamgrace@naver.com>
2022-02-10 20:43:27 +0900

    fix : 알림 api 주어지는 칼람값 변경

--- src/main/java/com/mumulcom/mumulcom/src/notice/dao/NoticeDao.java
+++ src/main/java/com/mumulcom/mumulcom/src/notice/dao/NoticeDao.java
@@ -24,28 +24,42 @@ public class NoticeDao {
     public List<GetNoticeRes> noticeList (long userIdx) {
 
         String noticeListQuery = "select n.questionIdx, noticeContent, profileImgUrl, (select CASE\n" +
-                "\twhen((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 0) then '오늘'\n" +
-                "    when((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 1) then '어제'\n" +
-                "    when((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 2) then '2일전'\n" +
-                "    when((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 3) then '3일전'\n" +
-                "    when((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 4) then '4일전'\n" +
-                "    when((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 5) then '5일전'\n" +
-                "    when((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 6) then '6일전'\n" +
-                "    when((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) >= 7 and (select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) <= 13) then '1주전'\n" +
-                "    when((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) >= 14 and (select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) <= 20) then '2주전'\n" +
-                "    when((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) >= 21 and (select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) <= 27) then '3주전'\n" +
-                "    when((select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) >= 28 and (select timestampdiff(DAY,DATE_FORMAT(createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) <= 59) then '1달전'\n" +
+                "\twhen((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 0) then '오늘'\n" +
+                "    when((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 1) then '어제'\n" +
+                "    when((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 2) then '2일전'\n" +
+                "    when((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 3) then '3일전'\n" +
+                "    when((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 4) then '4일전'\n" +
+                "    when((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 5) then '5일전'\n" +
+                "    when((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) = 6) then '6일전'\n" +
+                "    when((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) >= 7 and (select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) <= 13) then '1주전'\n" +
+                "    when((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) >= 14 and (select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) <= 20) then '2주전'\n" +
+                "    when((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) >= 21 and (select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) <= 27) then '3주전'\n" +
+                "    when((select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) >= 28 and (select timestampdiff(DAY,DATE_FORMAT(n.createdAt, '%y-%m-%d'),DATE_FORMAT(now(), '%y-%m-%d'))) <= 59) then '1달전'\n" +
                 "    else '3달 넘은 오래된 게시물' \n" +
-                "end) as diffTime,createdAt\n" +
-                "from Notice n join (select profileImgUrl, q.questionIdx from Question q join User u on q.userIdx = u.userIdx) writerInfo on n.questionIdx = writerInfo.questionIdx\n" +
-                "where n.userIdx = ?\n" +
-                "order by updatedAt desc";
+                "end) as diffTime,bigCategoryName, type\n" +
+                "from \n" +
+                "(select profileImgUrl, q.questionIdx, bigCategoryName, type, n.createdAt, noticeContent\n" +
+                "from\n" +
+                "(select profileImgUrl, q.questionIdx, bigCategoryName, ifnull(1,0) as type\n" +
+                "from Question q, User u, BigCategory b, CodeQuestion c\n" +
+                "where q.userIdx = u.userIdx and q.bigCategoryIdx = b.bigCategoryIdx and c.questionIdx = q.questionIdx) q, Notice n \n" +
+                "where n.questionIdx = q.questionIdx and userIdx = ?\n" +
+                "union all\n" +
+                "select profileImgUrl, q.questionIdx, bigCategoryName, type , n.createdAt, noticeContent\n" +
+                "from\n" +
+                "(select profileImgUrl, q.questionIdx, bigCategoryName, ifnull(2,0) as type\n" +
+                "from Question q, User u, BigCategory b, ConceptQuestion c\n" +
+                "where q.userIdx = u.userIdx and q.bigCategoryIdx = b.bigCategoryIdx and c.questionIdx = q.questionIdx) q, Notice n \n" +
+                "where n.questionIdx = q.questionIdx and userIdx = ?) n\n" +
+                "order by n.createdAt desc";
         return jdbcTemplate.query(noticeListQuery,
                 (rs,rowNum) -> new GetNoticeRes(
                         rs.getLong("questionIdx"),
                         rs.getString("profileImgUrl"),
                         rs.getString("noticeContent"),
-                        rs.getString("diffTime")
-                ),userIdx);
+                        rs.getString("diffTime"),
+                        rs.getString("bigCategoryName"),
+                        rs.getInt("type")
+                ),userIdx, userIdx);
     }
 }
