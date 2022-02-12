a3838f1f076a08060d98acc4e0fea2354c44554f
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-02-12 15:10:07 +0900

    Refactor: LocalDateTime -> ZonedDateTime

--- src/main/java/com/mumulcom/mumulcom/src/user/domain/BaseTimeEntity.java
+++ src/main/java/com/mumulcom/mumulcom/src/user/domain/BaseTimeEntity.java
@@ -2,24 +2,26 @@ package com.mumulcom.mumulcom.src.user.domain;
 
 
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.ZonedDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
+    protected ZonedDateTime createdAt;
+    protected ZonedDateTime updatedAt;
 
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = ZonedDateTime.now();
+        this.updatedAt = ZonedDateTime.now();
+    }
 
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = ZonedDateTime.now();
+    }
 }
